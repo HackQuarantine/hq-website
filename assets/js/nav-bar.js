1dd962c619358d0b1769378c32e1ca6240457fc2
Daniel <danielfspencer1@gmail.com>
2020-03-16 19:13:29 +0000

    Implement mobile version of nav bar

--- assets/js/nav-bar.js
+++ assets/js/nav-bar.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const pull = document.querySelector('.nav-content nav #pull')
+  const pull = document.querySelector('.nav-content #pull')
   const menu = document.querySelector('.nav-content nav ul')
 
   pull.addEventListener('click', () => {
